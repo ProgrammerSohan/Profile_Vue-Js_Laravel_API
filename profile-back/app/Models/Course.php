a2a0aaecf61bb7258fe2f9c6feb0247901298015
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-22 07:14:34 +0600

    model & migrations

--- profile-back/app/Models/Course.php
+++ profile-back/app/Models/Course.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
     use HasFactory;
+    public $table = "courses";
+    protected $fillable = ["*"];
+    public function students(){
+        return $this->belongsToMany(Student::class,"course_student");
+    }
 }
--- profile-back/app/Models/Student.php
+++ profile-back/app/Models/Student.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
     use HasFactory;
+    public $table = "students";
+    protected $fillable = ["*"];
+    public function courses(){
+        return $this->belongsToMany(Course::class,"course_student");
+    }
 }
